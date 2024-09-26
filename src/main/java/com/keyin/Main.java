23350a931e53a34331959e9c27f6b5b49b795d42
espurrell <150044207+espurrell@users.noreply.github.com>
2024-09-26 19:03:34 -0230

    Commit LinkedList

---
+++ src/main/java/com/keyin/Main.java
@@ -0,0 +1,16 @@
+package com.keyin;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.insertInLinkedList(1, 0);
+        singleLinkedList.insertInLinkedList(2, 1);
+        singleLinkedList.insertInLinkedList(3, 2);
+        singleLinkedList.insertInLinkedList(4, 3);
+        singleLinkedList.insertInLinkedList(5, 1);
+        singleLinkedList.traverseLinkedList();
+        singleLinkedList.searchNode(4);
+        singleLinkedList.deleteNode(3);
+        singleLinkedList.traverseLinkedList();
+    }
+}
